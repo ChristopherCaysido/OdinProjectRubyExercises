b2d4201adf9beecb3a4f5c7f9550e8807b148141
KatsuKira <christopher.caysido@gmail.com>
2024-06-20 01:36:43 +0800

    Finished the Second Exercise

--- Supplementary Exercises/Hashes/Exercise2.rb
+++ Supplementary Exercises/Hashes/Exercise2.rb
@@ -1,3 +1,23 @@
 # Look at Ruby's merge method. Notice that it has two versions. 
 # What is the difference between merge and merge!? 
 # Write a program that uses both and illustrate the differences.
+
+# Defining the Hashes
+
+consoles = {
+    Sony: "Playstation",
+    Microsoft: "Xbox",
+    Nintendo: "Switch"
+}
+
+computer = {
+    Desktop: "Gaming PC"
+}
+
+nondestructivemerge = consoles.merge(computer)
+puts " Used merge method: #{nondestructivemerge}"
+puts " State of the consoles hash #{consoles}"
+
+destructivemerge = consoles.merge!(computer)
+puts " Used merge! method: #{destructivemerge}"
+puts " State of the consoles hash #{consoles}"
