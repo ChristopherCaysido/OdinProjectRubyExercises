2080c9ef2498725b766ab6920e4e03b7f42f00b6
KatsuKira <christopher.caysido@gmail.com>
2024-06-21 02:22:56 +0800

    Completed Exercise 3

--- Supplementary Exercises/Hashes/Exercise3.rb
+++ Supplementary Exercises/Hashes/Exercise3.rb
@@ -1,3 +1,19 @@
 # Using some of Ruby's built-in Hash methods, write a program that loops through a hash and prints all of the keys. 
 # Then write a program that does the same thing except printing the values. Finally, write a program that prints both.
 
+sample_hash = {
+    Fromsoft: "Elden Ring",
+    Bungie: "Destiny",
+    Capcom: "Resident Evil"
+}
+
+sample_hash.each do 
+    |k,v| puts "#{k}"
+end
+
+sample_hash.each do 
+    |k,v| puts "#{v}"
+end
+
+sample_hash.each_key {|key| puts "#{key}" }
+sample_hash.each_value {|value| puts "#{value}" }
